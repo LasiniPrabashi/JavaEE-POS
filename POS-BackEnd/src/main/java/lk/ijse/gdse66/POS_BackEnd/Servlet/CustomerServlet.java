9169f60e50ee4d7cef66d800abe9a3f47abc08fc
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-01-23 00:31:58 +0530

    update

--- POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
+++ POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse66.POS_BackEnd.Servlet;
 
 import java.io.*;
+import java.sql.Connection;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,13 +11,21 @@ import javax.sql.DataSource;
 @WebServlet(name = "CustomerServlet", urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
-
+    @Resource(name = "java:comp/env/jdbc/pool")
     DataSource dataSource;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+
+        Connection connection = null;
+
         resp.addHeader("Access-Control-Allow-Origin", "*");
+
+
+
     }
 
     public void destroy() {
