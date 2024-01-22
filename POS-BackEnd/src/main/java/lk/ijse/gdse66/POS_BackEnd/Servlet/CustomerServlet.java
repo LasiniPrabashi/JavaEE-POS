f08f7258c327285460ecc42540a1fa765648704d
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-01-22 23:27:30 +0530

    update

--- POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
+++ POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
@@ -4,10 +4,13 @@ import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.DataSource;
 
 @WebServlet(name = "CustomerServlet", urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+
+    DataSource dataSource;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
