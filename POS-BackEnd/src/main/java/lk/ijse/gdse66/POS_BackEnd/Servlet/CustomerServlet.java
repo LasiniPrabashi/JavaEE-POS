162a80bbb96d13fbc61851564019907684aa933a
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-01-22 15:05:52 +0530

    update

--- POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
+++ POS-BackEnd/src/main/java/lk/ijse/gdse66/POS_BackEnd/Servlet/CustomerServlet.java
@@ -1,25 +1,18 @@
 package lk.ijse.gdse66.POS_BackEnd.Servlet;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "CustomerServlet", value = "/customer-servlet")
+@WebServlet(name = "CustomerServlet", urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
     }
 
     public void destroy() {
