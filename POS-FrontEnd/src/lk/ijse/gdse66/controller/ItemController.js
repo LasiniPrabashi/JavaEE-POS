ac9cb4766c689dd084874cfe636394821df58636
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-02-01 15:02:38 +0530

    creat itemController done

---
+++ POS-FrontEnd/src/lk/ijse/gdse66/controller/ItemController.js
@@ -0,0 +1,72 @@
+loadAllItem();
+
+$("#btnItem").click(function () {
+    let itemOb = {
+        "itemCode": $("#txtItemId").val(),
+        "itemName": $("#txtItemName").val(),
+        "itemQty": $("#txtQty").val(),
+        "itemPrice": $("#txtPrice").val()
+    };
+
+    $.ajax({
+        url: "http://localhost:8080/backEnd/item",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(itemOb),
+        success: function (res) {
+            if (res.status == 200) {
+                loadAllItem();
+                alert(res.message);
+                resetItem();
+            } else {
+                alert(res.data);
+            }
+        },
+        error: function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+});
+
+$("#btnGetAllItem").click(function () {
+    resetItem();
+    loadAllItem();
+});
+
+function resetItem() {
+    $("#txtItemId").val("");
+    $("#txtItemName").val("");
+    $("#txtQty").val("");
+    $("#txtPrice").val("");
+}
+
+function loadAllItem() {
+    $("#itemTable").empty();
+    $.ajax({
+        url: "http://localhost:8080/backEnd/item?option=GETALL",
+        method: "GET",
+        success: function (resp) {
+            for (const item of resp.data) {
+                let row = `<tr><td>${item.itemCode}</td><td>${item.itemName}</td><td>${item.itemQty}</td><td>${item.itemPrice}</td></tr>`;
+                $("#itemTable").append(row);
+            }
+            bindClickEvent();
+        }
+    });
+}
+
+function bindClickEvent() {
+    $("#itemTable>tr").click(function () {
+        let id = $(this).children().eq(0).text();
+        let name = $(this).children().eq(1).text();
+        let qtyOnHand = $(this).children().eq(2).text();
+        let price = $(this).children().eq(3).text();
+
+        $("#txtItemId").val(id);
+        $("#txtItemName").val(name);
+        $("#txtQty").val(qtyOnHand);
+        $("#txtPrice").val(price);
+    });
+}
